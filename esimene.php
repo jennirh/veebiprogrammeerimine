5ebbf675a2b4ecbaa3aa45ddd9d4bcea6fed879a
Jennifer Heinsalu <heinjenn@greeny.cs.tlu.ee>
2017-09-12 14:25:41 +0300

    esimene

---
+++ esimene.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jennifer Heinsalu</title>
+</head>
+<body>
+	<h1>Jenniferi veebileht</h1>
+	<p>Stop checking out my site! <br>
+	See leht on loodud õpptetöö raames ning ei sisalda mingit tõsiseltvõetavat sisu<p>
+
+</body>
+</html>
